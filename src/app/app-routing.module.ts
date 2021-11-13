325de0fddf7a68cdc04560df6a5602f86812e9af
Bruno <brunolorimier@hotmail.com>
2021-11-12 21:47:59 -0300

    Roteamento melhorado, 2 botões com padding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,22 @@ import { MenuComponent } from './menu/menu-usuario/menu.component';
 import { PresencaComponent } from './presenca/presenca.component';
 import { DashComponent } from './dash/dash.component';
 import { TermosComponent } from './termos/termos.component';
+import { ModeradorMenuComponent } from './menu/menu-moderador/menu-moderador.component';
+import { AdmMenuComponent } from './menu/menu-adm/menu-adm.component';
 
 const routes: Routes = [
-  {path:'', component: DashComponent},
+  // {path:'', component: DashComponent},
   {path:'evento', component: PresencaComponent},
-  {path: 'termos', component: TermosComponent}
+  {path: 'termos', component: TermosComponent},
+  {path: '', component: MenuComponent, children: [
+    {path: '', component: DashComponent}, 
+  ]},
+  {path: 'adm', component: AdmMenuComponent, children: [
+    {path: '', component: DashComponent}, 
+  ]},
+  {path: 'mod', component: ModeradorMenuComponent, children: [
+    {path: '', component: DashComponent}
+  ]}
 ];
 
 @NgModule({
